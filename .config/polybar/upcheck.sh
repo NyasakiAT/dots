15314e2f5187151c0a3191095a7f2b8ca858c91d
Marcel Wall <walk.marcel.97@googlemail.com>
2022-06-16 16:11:32 +0200

    Updated screenshot tool

--- .config/polybar/upcheck.sh
+++ .config/polybar/upcheck.sh
@@ -3,7 +3,9 @@
 echo "CHECKING"
 if timeout 1 ping -c 1 1.1.1.1 &> /dev/null
 then
-	echo "%{F#ddffa7}UP%{F-}"
+	echo "%{F#ddffa7}Online%{F-}"
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')]	1" >> ./uptime.txt
 else
-	echo "%{F#ff8b92}DOWN%{F-}"
+	echo "%{F#ff8b92}Offline%{F-}"
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')]	0" >> ./uptime.txt
 fi
