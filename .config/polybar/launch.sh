f4c6f007bfefee26efe69294fd7535610f3488d0
Marcel Walk <walk.marcel.97@googlemail.com>
2022-07-03 15:14:51 +0200

    Reworked Rice

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
 
-# Launch
-polybar main &
+echo "Bars launched..."
