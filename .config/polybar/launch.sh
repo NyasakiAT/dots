238ac82a3f8aeb1fb7b909a984fff8251f29fa47
Marcel Walk <walk.marcel.97@googlemail.com>
2022-07-03 20:26:56 +0200

    Fixed some stuff

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,12 +6,6 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
-else
-  polybar --reload main &
-fi
+polybar --reload main &
 
 echo "Bars launched..."
